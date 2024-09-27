195181039ff3a4228bf2a0557ddb2e10f3b4acc2
zapirus <ibragimov-009@mail.ru>
2024-09-27 19:05:42 +0300

    ts2

--- t2/db.go
+++ t2/db.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+)
+
+// переменная, через которую мы будем работать с БД
+var DB *gorm.DB
+
+func InitDB() {
+	// в dsn вводим данные, которые мы указали при создании контейнера
+	dsn := "host=localhost user=postgres password=9990 dbname=postgres port=5433 sslmode=disable"
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database: ", err)
+	}
+}
--- t2/main.go
+++ t2/main.go
@@ -1,1 +1,46 @@
-package t2
+package main
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func main() {
+	// Вызываем метод InitDB() из файла db.go
+	InitDB()
+
+	// Автоматическая миграция модели Message
+	err := DB.AutoMigrate(&Message{})
+	if err != nil {
+		return
+	}
+
+	router := mux.NewRouter()
+	router.HandleFunc("/api/messages", CreateMessage).Methods("POST")
+	router.HandleFunc("/api/messages", GetMessages).Methods("GET")
+	http.ListenAndServe(":8080", router)
+}
+
+func CreateMessage(w http.ResponseWriter, r *http.Request) {
+
+	var msg Message
+	err := json.NewDecoder(r.Body).Decode(&msg)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	DB.Create(&msg)
+
+}
+
+func GetMessages(w http.ResponseWriter, r *http.Request) {
+
+	var messages []Message
+	DB.Find(&messages)
+	err := json.NewEncoder(w).Encode(messages)
+	if err != nil {
+		return
+	}
+
+}
